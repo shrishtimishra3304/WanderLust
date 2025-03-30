1bdc345c76d914ec14cece2543041ab879c7094b
chandani <shrishti3304@gmail.com>
2025-03-30 12:16:52 +0530

    modified-edit-page

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));console.log('Server is running on port 3000');
+console.log('Connected to MongoDB');
+console.log('Error connecting to MongoDB:');
+console.log("Sample listing saved");
+console.log("successful testing");
 
 
 // 1.home route
